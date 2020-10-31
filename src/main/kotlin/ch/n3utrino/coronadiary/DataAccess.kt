bacd68beaa794de336cf71a87a7a55511268cbc1
Gabe Kälin <gabe@dehei.me>
2020-10-31 10:15:24 +0100

    fix: production redis config

--- src/main/kotlin/ch/n3utrino/coronadiary/DataAccess.kt
+++ src/main/kotlin/ch/n3utrino/coronadiary/DataAccess.kt
@@ -3,22 +3,23 @@ package ch.n3utrino.coronadiary
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.annotation.Id
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import redis.clients.jedis.JedisPool
 import redis.clients.jedis.JedisPoolConfig
+import java.net.URI
 import java.time.LocalDate
 
 
 @Configuration
 @EnableRedisRepositories
-internal class AppConfig {
+internal class RedisConfig {
 
     @Bean
-    fun jedisConnectionFactory(): RedisConnectionFactory? {
+    fun jedisPool(): JedisPool {
+        val redisURI = URI(System.getenv("REDIS_URL") ?: "redis://localhost:6379")
         val poolConfig = JedisPoolConfig()
         poolConfig.maxTotal = 10
         poolConfig.maxIdle = 5
@@ -26,9 +27,8 @@ internal class AppConfig {
         poolConfig.testOnBorrow = true
         poolConfig.testOnReturn = true
         poolConfig.testWhileIdle = true
-        return JedisConnectionFactory(poolConfig)
+        return JedisPool(poolConfig, redisURI)
     }
-
 
 }
 
